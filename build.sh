c9f6863aedfca503474371136e412dca8d06150f
Mark Huang <mlhuang@cs.princeton.edu>
2006-03-29 19:19:55 +0000

    - move vserver-reference group definition into this script instead of   relying upon it living in the yumgroups.xml file

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2004-2006 The Trustees of Princeton University
 #
-# $Id: build.sh,v 1.4 2006/03/21 14:57:29 mlhuang Exp $
+# $Id: build.sh,v 1.5 2006/03/29 17:08:45 mlhuang Exp $
 #
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
@@ -26,6 +26,35 @@ export PATH
 # Release and architecture to install
 releasever=2
 basearch=i386
+
+# Packages to install
+packagelist=(
+bash
+coreutils
+iputils
+kernel-vserver
+bzip2
+crontabs
+diffutils
+logrotate
+openssh-clients
+passwd
+rsh
+rsync
+sudo
+tcpdump
+telnet
+traceroute
+time
+vixie-cron
+wget
+yum
+curl
+gzip
+perl
+python
+tar
+)
 
 usage()
 {
@@ -56,9 +85,14 @@ vroot=$PWD/vservers/vserver-reference
 install -d -m 755 $vroot
 
 # Install vserver-reference system
-mkfedora -v -r $releasever -a $basearch -g VServer $vroot
+for package in "${packagelist[@]}" ; do
+    packages="$packages -p $package"
+done
+mkfedora -v -r $releasever -a $basearch $packages $vroot
 
 # This tells the Boot Manager that it is okay to update
 # /etc/resolv.conf and /etc/hosts whenever the network configuration
 # changes. Users are free to delete this file.
 touch $vroot/etc/AUTO_UPDATE_NET_FILES
+
+exit 0
